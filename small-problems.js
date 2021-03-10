977d179d1ea73d7dd94c97b4f81e45aa1d6da3a2
samergain <samersadon@gmail.com>
2021-03-09 19:14:58 -0500

    adding problem 18.

--- small-problems.js
+++ small-problems.js
@@ -380,3 +380,25 @@ var isAnagram = function(strA, strB) {
     }
     return true;
 };
+
+
+/////////   PROBLEM 18  //////////
+// Write code to create a function that accepts an array of numbers
+// Each number represents the duration of a song in minutes
+// Return the maximum number of songs from the list that can fit in a 60 minute playlist
+
+//V1
+var mostSongsInPlaylist = function(arr) {
+    // add your code here...
+    let sorted = arr.sort();
+    let sum = 0;
+    for(let i=0; i<arr.length; i++){
+      sum += arr[i];
+      if(sum > 60){
+        return i;
+      }
+    }
+    return arr.length;
+  };
+
+//
