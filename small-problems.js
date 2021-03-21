f4e60d4578e5a99141fe3bb015f1a13c62a18356
samergain <samersadon@gmail.com>
2021-03-20 21:11:42 -0400

    adding problem31

--- small-problems.js
+++ small-problems.js
@@ -849,3 +849,31 @@ function asc_sqr(arr){
     }
     return result;
 }
+
+///////////     Problem 31      ///////////
+// Write code to create a function that accepts two strings
+// If the second string is found inside of the first string, return its starting index in the first string
+// Else return `-1`
+
+var strStr = function(str1, str2) {
+    if (str2 === "") {
+      return 0;
+    }
+  
+    for (var i = 0; i < str1.length; i++) {
+      var char = str1[i];
+  
+      if (char === str2[0]) {
+        var j = i;
+  
+        var substr = str1.substring(i, i + str2.length);
+  
+        if (substr === str2) {
+          return i;
+        }
+      }
+    }
+  
+    return -1;
+  };
+  
