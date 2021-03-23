2c1509334d46acaaa8262919d345f0423a654266
samergain <samersadon@gmail.com>
2021-03-23 09:40:00 +0200

    adding problem34

--- small-problems.js
+++ small-problems.js
@@ -919,3 +919,48 @@ var missingNumber = function(nums) {
 // Write a function that takes an array and a positive integer and "rotates" the array to the left by the integer
 // Modify the original array rather than returning a new one
 // There is no need to return from this function
+var leftRotation = function(arr, positions) {
+    if (arr.length === 0) {
+      return;
+    }
+  
+    while (positions > 0) {
+      var first = arr.shift();
+      
+      arr.push(first);
+      
+      positions--;
+    }
+  };
+
+///////     PROBLEM 34      ////////
+// Write a function that takes two strings and determines returns true if every word found in the second string is present in the first string. You will be checking for both words and their frequency. Assume you'll need to worry about casing, but the strings won't contain any punctuation. Assume neither string will be empty
+
+var ransomNote = function(magazine, note) {
+    var magazineWords = magazine.split(" ");
+    var noteWords = note.split(" ");
+  
+    var magazineMap = {};
+    var noteMap = {};
+  
+    for (var i = 0; i < magazineWords.length; i++) {
+      var word = magazineWords[i];
+      magazineMap[word] = (magazineMap[word] || 0) + 1;
+      //the previous line is the same as:
+      //(magazineMap[word]) ? (magazineMap[word]++) : (magazineMap[word] = 1)
+    }
+  
+    for (var i = 0; i < noteWords.length; i++) {
+      var word = noteWords[i];
+      noteMap[word] = (noteMap[word] || 0) + 1;
+    }
+  
+    for (var key in noteMap) {
+      if (magazineMap[key] !== noteMap[key]) {
+        return false;
+      }
+    }
+  
+    return true;
+  };
+  
